e4c25abfac64e3154f5b0cfe5f9acaa52d24a3f2
mano8 <mex.serra@gmail.com>
2023-10-31 17:51:41 +0000

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,6 +12,13 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 
 //END_ASYNC
@@ -50,4 +58,4 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 
-app.listen(process.env.PORT || 3000, () => {});
+app.listen(process.env.PORT || 3000, () => { });
